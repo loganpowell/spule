6b9e26f509dbd13576792d00c66f22ce34bfceef
Logan Powell <loganp@tepper.cmu.edu>
2020-02-02 03:29:44 -0500

    ...

--- src/utils/diffKeys.js
+++ src/utils/diffKeys.js
@@ -1,12 +1,12 @@
-export function diffKeys(known_keys = [], known_obj = {}) {
-  let all = Object.keys(known_obj)
-  let uknown_keys = all.filter(key => !known_keys.includes(key))
-  let uknown_obj = Object.entries(known_obj).reduce((a, [k, v]) => {
-    if (!known_keys.includes(k)) return { ...a, [k]: v }
+export function diffKeys(nKeys = [], nObj = {}) {
+  let all = Object.keys(nObj)
+  let xKeys = all.filter(key => !nKeys.includes(key))
+  let xObj = Object.entries(nObj).reduce((a, [k, v]) => {
+    if (!nKeys.includes(k)) return { ...a, [k]: v }
     else return a
   }, {})
-  return [uknown_keys || [], uknown_obj || {}]
+  return [xKeys, xObj]
 }
 
-// keys_diff(["a", "b"], { a: 1, b: 2, c: 3, d: 4 })
-// => [ [ 'c', 'd' ], { c: 3, d: 4 } ]
+diffKeys(["a", "b"], { a: 1, b: 2, c: 3, d: 4 }) //?
+diffKeys([1, 2, 3]) //?
