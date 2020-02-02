4a8bace4697f20b44a8bceb65fecfa16e1a723ff
Logan Powell <loganp@tepper.cmu.edu>
2020-02-01 22:18:54 -0500

    la

--- examples/misc_docs/prefix_parsing.js
+++
@@ -1,7 +0,0 @@
-import { parse_URL } from "../../src/utils"
-
-parse_URL("https://loganpowell.github.io/ac/".replace(/ac\//g, "")) //?
-
-const escape = string => string.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&")
-
-new RegExp(escape("ac/"), "g") //?
--- examples/streaming_API/theme/index.js
+++
@@ -1,3 +0,0 @@
-import { theme } from "./uswds_theme"
-import { themer } from "styled-system-hdom"
-export const [theme_styler, THEME] = themer(theme)
