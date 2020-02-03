fe2a7660b248b510bf564048a8d5d909cbb17993
Logan Powell <loganp@tepper.cmu.edu>
2020-02-03 15:01:16 -0500

    modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /** @format */
-import {
+export {
   FLIP_FIRST,
   FLIP_LAST_INVERSE_PLAY,
   INJECT_HEAD,
@@ -8,7 +8,7 @@ import {
   SET_STATE
 } from "./commands"
 
-import {
+export {
   boot,
   registerCMD,
   registerRouter,
@@ -17,11 +17,11 @@ import {
   run$
 } from "./core"
 
-import { $store$, set$$tate } from "./store"
+export { $store$, set$$tate } from "./store"
 
-import { FLIPkid } from "./components"
+export { FLIPkid } from "./components"
 
-import {
+export {
   BODY_,
   DEFAULT_CFG,
   DOM_,
@@ -56,59 +56,4 @@ import {
   trace_
 } from "./constants.js"
 
-import { trace$, fURL, unfURL } from "./utils"
-
-module.exports = {
-  FLIP_FIRST,
-  FLIP_LAST_INVERSE_PLAY,
-  INJECT_HEAD,
-  HURL,
-  HURLer,
-  SET_STATE,
-  boot,
-  registerCMD,
-  registerRouter,
-  registerRouterDOM,
-  DOMnavigated$,
-  run$,
-  $store$,
-  set$$tate,
-  FLIPkid,
-  trace$,
-  fURL,
-  unfURL,
-  constants: {
-    BODY_,
-    DEFAULT_CFG,
-    DOM_,
-    HEAD_,
-    PAGE_TEMPLATE_,
-    ROOT_,
-    ROUTE_LOADING_,
-    ROUTE_PATH_,
-    URL_,
-    URL_data_,
-    URL_domain_,
-    URL_hash_,
-    URL_page_,
-    URL_path_,
-    URL_query_,
-    URL_subdomain_,
-    app_,
-    args_,
-    draft_,
-    erro_,
-    handler_,
-    post_,
-    prefix_,
-    prep_,
-    reso_,
-    root_,
-    router_,
-    run_,
-    source$_,
-    state_,
-    sub$_,
-    trace_
-  }
-}
+export { trace$, fURL, unfURL } from "./utils"
